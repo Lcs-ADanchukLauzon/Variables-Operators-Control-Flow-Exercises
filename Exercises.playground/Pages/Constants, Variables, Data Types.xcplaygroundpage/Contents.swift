dcc30d9a088e96aa2d97ee4886b9f1300255f6fe
Danchuk-Lauzon, Alexy <adanchuklauzon@lcs-mba3080.lcs.on.ca>
2017-12-13 10:17:31 -0500

    second question done

--- Exercises.playground/Pages/Constants, Variables, Data Types.xcplaygroundpage/Contents.swift
+++ Exercises.playground/Pages/Constants, Variables, Data Types.xcplaygroundpage/Contents.swift
@@ -50,7 +50,9 @@ let studentNovemberReportCard = 90
  
  */
 // Answer Question 2 below
-
+let myAge = 16
+let otherAge = 40.0
+let averageAge = (Double(myAge) + otherAge) / 2
 
 /*:
  ### Question 3
